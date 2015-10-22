dfe46916e76b2ea168aa6c8f698e9abe1bbf95a3
Jan Schnasse <jan@schnasse.eu>
2015-10-22 12:10:22 +0200

    set formatting to java conventions - remove .settings from .gitignore - reformat code

--- test/TestEbookConverter.java
+++ test/TestEbookConverter.java
@@ -1,4 +1,3 @@
-
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
@@ -34,111 +33,160 @@ import play.test.TestBrowser;
  */
 public class TestEbookConverter {
 
-	public static FakeApplication app;
+    public static FakeApplication app;
 
-	@BeforeClass
-	public static void startApp() {
-		app = Helpers.fakeApplication(Helpers.inMemoryDatabase());
-		Helpers.start(app);
+    @BeforeClass
+    public static void startApp() {
+	app = Helpers.fakeApplication(Helpers.inMemoryDatabase());
+	Helpers.start(app);
+    }
+
+    @AfterClass
+    public static void stopApp() {
+	Helpers.stop(app);
+    }
+
+    @Test
+    public void testClass() {
+	String inputDirectory = Thread.currentThread().getContextClassLoader()
+		.getResource("epub").getPath();
+	EbookConverter conv = new EbookConverter(inputDirectory);
+	File result = conv.convert("/tmp/rauls.epub");
+	result.deleteOnExit();
+	Assert.assertEquals(true, result.exists());
+    }
+
+    @Test
+    public void test_ebook_result() {
+	try {
+	    String inputDirectory = Thread.currentThread()
+		    .getContextClassLoader().getResource("epub").getPath();
+	    EbookConverter conv = new EbookConverter(inputDirectory);
+	    File result = conv.convert("/tmp/rauls.epub");
+	    result.deleteOnExit();
+	    Book readBook = new EpubReader().readEpub(new FileInputStream(
+		    result));
+
+	    System.out.println(readBook.getTitle());
+	    System.out.println(readBook.getResources().getAll());
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/ga.js"));
+	    System.out
+		    .println(readBook
+			    .getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/200px-Epub_logo_color-109x150.png"));
+	    System.out
+		    .println(readBook
+			    .getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/200px-Epub_logo_color.png"));
+	    System.out
+		    .println(readBook
+			    .getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/Alices-Adventures-in-Wonderland_2011-01-30_18-17-30-300x181.png"));
+	    System.out
+		    .println(readBook
+			    .getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/external-tracking.js"));
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/init.js"));
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/jquery-migrate.js"));
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/jquery.js"));
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/style.css"));
+	    System.out.println(readBook.getResources().getByHref(
+		    "Epublib – a java epub library_files/wp-emoji-release.js"));
+
+	    // ----------------ASSERTS------------------
+
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources().getByHref(
+			    "Epublib – a java epub library_files/ga.js") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/200px-Epub_logo_color-109x150.png") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/200px-Epub_logo_color.png") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/Alices-Adventures-in-Wonderland_2011-01-30_18-17-30-300x181.png") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/external-tracking.js") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources().getByHref(
+			    "Epublib – a java epub library_files/init.js") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/jquery-migrate.js") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources().getByHref(
+			    "Epublib – a java epub library_files/jquery.js") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources().getByHref(
+			    "Epublib – a java epub library_files/style.css") != null);
+	    Assert.assertEquals(
+		    true,
+		    readBook.getResources()
+			    .getByHref(
+				    "Epublib – a java epub library_files/wp-emoji-release.js") != null);
+
+	} catch (Exception e) {
+	    throw new RuntimeException(e);
 	}
+    }
 
-	@AfterClass
-	public static void stopApp() {
-		Helpers.stop(app);
-	}
+    @Test
+    public void testAll() throws FileNotFoundException, IOException {
+	WebDownloader dwnl = new WebDownloader(
+		"http://www.tutorialspoint.com/java/java_string_substring.htm");
+	File dir = dwnl.download("tmp.html");
+	dir.deleteOnExit();
+	EbookConverter conv = new EbookConverter(dir.getAbsolutePath());
+	File result = conv.convert(File.createTempFile("ebook", ".epub")
+		.getAbsolutePath());
+	result.deleteOnExit();
+	Assert.assertEquals(true, result.exists());
+	Assert.assertEquals(true, dir.exists());
+	System.out.println(result.toString());
+    }
 
-	@Test
-	public void testClass() {
-		String inputDirectory = Thread.currentThread().getContextClassLoader().getResource("epub").getPath();
-		EbookConverter conv = new EbookConverter(inputDirectory);
-		File result = conv.convert("/tmp/rauls.epub");
-		result.deleteOnExit();
-		Assert.assertEquals(true, result.exists());
-	}
+    @Test
+    public void testDownloader() {
+	WebDownloader dwnl = new WebDownloader(
+		"http://www.tutorialspoint.com/java/java_string_substring.htm");
+	File dir = dwnl.download("tmp.html");
+	dir.deleteOnExit();
+    }
 
-	@Test
-	public void test_ebook_result() {
-		try {
-			String inputDirectory = Thread.currentThread().getContextClassLoader().getResource("epub").getPath();
-			EbookConverter conv = new EbookConverter(inputDirectory);
-			File result = conv.convert("/tmp/rauls.epub");
-			result.deleteOnExit();
-			Book readBook = new EpubReader().readEpub(new FileInputStream(result));
+    @Test
+    public void confTest() throws IOException {
+	Properties properties = new Properties();
+	properties.load(Play.application()
+		.resourceAsStream("config.properties"));
+	System.out.println(properties.getProperty("fedoraUrl"));
+	System.out.println(Play.application().getFile("").getAbsolutePath());
 
-			System.out.println(readBook.getTitle());
-			System.out.println(readBook.getResources().getAll());
-			System.out.println(readBook.getResources().getByHref("Epublib – a java epub library_files/ga.js"));
-			System.out.println(readBook.getResources()
-					.getByHref("Epublib – a java epub library_files/200px-Epub_logo_color-109x150.png"));
-			System.out.println(
-					readBook.getResources().getByHref("Epublib – a java epub library_files/200px-Epub_logo_color.png"));
-			System.out.println(readBook.getResources().getByHref(
-					"Epublib – a java epub library_files/Alices-Adventures-in-Wonderland_2011-01-30_18-17-30-300x181.png"));
-			System.out.println(
-					readBook.getResources().getByHref("Epublib – a java epub library_files/external-tracking.js"));
-			System.out.println(readBook.getResources().getByHref("Epublib – a java epub library_files/init.js"));
-			System.out.println(
-					readBook.getResources().getByHref("Epublib – a java epub library_files/jquery-migrate.js"));
-			System.out.println(readBook.getResources().getByHref("Epublib – a java epub library_files/jquery.js"));
-			System.out.println(readBook.getResources().getByHref("Epublib – a java epub library_files/style.css"));
-			System.out.println(
-					readBook.getResources().getByHref("Epublib – a java epub library_files/wp-emoji-release.js"));
-
-			// ----------------ASSERTS------------------
-
-			Assert.assertEquals(true,
-					readBook.getResources().getByHref("Epublib – a java epub library_files/ga.js") != null);
-			Assert.assertEquals(true, readBook.getResources()
-					.getByHref("Epublib – a java epub library_files/200px-Epub_logo_color-109x150.png") != null);
-			Assert.assertEquals(true, readBook.getResources()
-					.getByHref("Epublib – a java epub library_files/200px-Epub_logo_color.png") != null);
-			Assert.assertEquals(true, readBook.getResources().getByHref(
-					"Epublib – a java epub library_files/Alices-Adventures-in-Wonderland_2011-01-30_18-17-30-300x181.png") != null);
-			Assert.assertEquals(true, readBook.getResources()
-					.getByHref("Epublib – a java epub library_files/external-tracking.js") != null);
-			Assert.assertEquals(true,
-					readBook.getResources().getByHref("Epublib – a java epub library_files/init.js") != null);
-			Assert.assertEquals(true,
-					readBook.getResources().getByHref("Epublib – a java epub library_files/jquery-migrate.js") != null);
-			Assert.assertEquals(true,
-					readBook.getResources().getByHref("Epublib – a java epub library_files/jquery.js") != null);
-			Assert.assertEquals(true,
-					readBook.getResources().getByHref("Epublib – a java epub library_files/style.css") != null);
-			Assert.assertEquals(true, readBook.getResources()
-					.getByHref("Epublib – a java epub library_files/wp-emoji-release.js") != null);
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Test
-	public void testAll() throws FileNotFoundException, IOException {
-		WebDownloader dwnl = new WebDownloader("http://www.tutorialspoint.com/java/java_string_substring.htm");
-		File dir = dwnl.download("tmp.html");
-		dir.deleteOnExit();
-		EbookConverter conv = new EbookConverter(dir.getAbsolutePath());
-		File result = conv.convert(File.createTempFile("ebook", ".epub").getAbsolutePath());
-		result.deleteOnExit();
-		Assert.assertEquals(true, result.exists());
-		Assert.assertEquals(true, dir.exists());
-		System.out.println(result.toString());
-	}
-
-	@Test
-	public void testDownloader() {
-		WebDownloader dwnl = new WebDownloader("http://www.tutorialspoint.com/java/java_string_substring.htm");
-		File dir = dwnl.download("tmp.html");
-		dir.deleteOnExit();
-	}
-
-	@Test
-	public void confTest() throws IOException {
-		Properties properties = new Properties();
-		properties.load(Play.application().resourceAsStream("config.properties"));
-		System.out.println(properties.getProperty("fedoraUrl"));
-		System.out.println(Play.application().getFile("").getAbsolutePath());
-
-	}
+    }
 
 }
